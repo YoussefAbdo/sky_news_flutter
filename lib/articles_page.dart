e6b4f9751a4b1682199b0098c9b65e72b34cb129
YoussefAbdo <mryoussefjoe@gmail.com>
2019-03-13 02:03:07 +0200

    HTTP Request

--- lib/articles_page.dart
+++ lib/articles_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert' as convert;
 
 class ArticlesPage extends StatefulWidget {
   @override
@@ -6,15 +9,34 @@ class ArticlesPage extends StatefulWidget {
 }
 
 class _ArticlesPage extends State<ArticlesPage> {
+
+  String url = 'https://newsapi.org/v2/top-headlines?country=eg&apiKey=597ac250047845ad8e1fa41510638df2';
+
+  Future<String> makeRequest() async {
+    var response = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
+    //print(response.body);
+    List data;
+
+    var jsonResponse  = convert.jsonDecode(response.body);
+    data = jsonResponse ["results"];
+    var itemCount = jsonResponse['totalResults'];
+    print("Number of books about http: $itemCount.");
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: Scaffold(
           appBar: new AppBar(
-            title: new Text('Articles Page'),
+            title: new Text('Articles'),
           ),
           body: Center(
-            child: Text('This application is made by Youssef'),
+            child: new RaisedButton(
+    child: new Text('Make Request'),
+    onPressed: makeRequest,
+            )
           )
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Sky News',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -39,6 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      body: new ArticlesPage(),
       appBar: new AppBar(title: new Text('Sky News Articles')),
       drawer: new Drawer(
         child: ListView(
