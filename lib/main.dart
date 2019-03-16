91698efcedb168bcd655628a25f010dd2f9b52f8
YoussefAbdo <mryoussefjoe@gmail.com>
2019-03-16 15:34:45 +0200

    Adding Loader Indicator

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,12 @@ import 'dart:async';
 import 'dart:convert' as convert;
 import 'package:firebase_admob/firebase_admob.dart';
 import 'dart:io' show Platform;
-//import 'package:fluttertoast/fluttertoast.dart';
-//import 'package:async_loader/async_loader.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-
 
 //pages
 import './about_page.dart';
-import './articles_page.dart';
-import './argaam_page.dart';
 import './description_page.dart';
+import './loader.dart';
 
  final String adUnitId = Platform.isAndroid
     ? 'ca-app-pub-3940256099942544/1033173712'
@@ -24,6 +20,8 @@ import './description_page.dart';
     : "ca-app-pub-7444470694788180~4898078390";
 
   Timer timer;
+  String url = 'https://newsapi.org/v2/top-headlines?country=ar&apiKey=597ac250047845ad8e1fa41510638df2';
+  String appBarTitle = 'Flutter News';
 
 MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
   keywords: <String>['flutterio', 'beautiful apps'],
@@ -32,12 +30,10 @@ MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
   testDevices: <String>[], // Android emulators are considered test devices
 );
 
-
-
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatefulWidget {
+
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -55,7 +51,6 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => new _MyHomePageState();
@@ -63,18 +58,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  String url = 'https://newsapi.org/v2/top-headlines?country=ar&apiKey=597ac250047845ad8e1fa41510638df2';
-  List data;
-
-  Future<String> makeRequest() async {
+  //List data;
+  Future<List> _makeRequest() async {
     var response = await http
         .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+    List data;
 
     setState(() {
       var jsonResponse  = convert.jsonDecode(response.body);
       data = jsonResponse ["articles"];
-
     });
+
+    return data;
 
   }
 
@@ -84,76 +79,55 @@ class _MyHomePageState extends State<MyHomePage> {
     FirebaseAdMob.instance.initialize(appId: adAppId);
     final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
     _firebaseMessaging.requestNotificationPermissions();
-    this.makeRequest();
-    timer = Timer.periodic(Duration(seconds: 10), (Timer t) => showAd());
+    this._makeRequest();
+    //timer = Timer.periodic(Duration(seconds: 10), (Timer t) => showAd());
 
-  }
-
-//  void showToastMessage(String message) {
-//    Fluttertoast.showToast(
-//        msg: message,
-//        toastLength: Toast.LENGTH_SHORT,
-//        gravity: ToastGravity.BOTTOM,
-//        timeInSecForIos: 1,
-//        backgroundColor: Colors.blue,
-//        textColor: Colors.white,
-//        fontSize: 12.0
-//    );
-//  }
-
-  void showAd () {
-
-    InterstitialAd myInterstitial = InterstitialAd(
-      // Replace the testAdUnitId with an ad unit id from the AdMob dash.
-      // https://developers.google.com/admob/android/test-ads
-      // https://developers.google.com/admob/ios/test-ads
-      adUnitId: adUnitId,
-      targetingInfo: targetingInfo,
-      listener: (MobileAdEvent event) {
-        print("InterstitialAd event is $event");
-      },
-    );
-
-    myInterstitial
-      ..load()
-      ..show(
-        anchorType: AnchorType.bottom,
-        anchorOffset: 0.0,
-      );
-
-    myInterstitial.dispose();
-    }
-
-
-  @override
-  void dispose() {
-    timer?.cancel();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body:new ListView.separated(
-          itemCount: data == null? 0:data.length,
-          separatorBuilder: (context, i) =>Divider(
-            color: Colors.black
-          ),
-          itemBuilder: (BuildContext context, i) {
-            return new ListTile(
-              title: new Text(data[i]["title"],
-                textDirection: TextDirection.rtl,),
-              trailing: new CircleAvatar(
-                backgroundImage: new NetworkImage(data[i]["urlToImage"]),
-              ),
-              onTap: () {
-                Navigator.push(context,
-                    new MaterialPageRoute(builder: (BuildContext context) =>
-                    new DescriptionPage(data[i])));
-              },
-            );
-          }),
-      appBar: new AppBar(title: new Text('Egypt News')),
+      body:
+        Container(
+          child: FutureBuilder(
+            future: this._makeRequest(),
+              builder: (BuildContext context, AsyncSnapshot asyncSnapshot) {
+              if (asyncSnapshot.data == null) {
+                return Container(
+                  child: Center(
+                    child: ColorLoader3()
+                  )
+                );
+              }
+              else {
+                return new ListView.separated(
+                    itemCount: asyncSnapshot.data.length,
+                    separatorBuilder: (context, i) =>
+                        Divider(
+                            color: Colors.black
+                        ),
+                    itemBuilder: (BuildContext context, i) {
+                      return new ListTile(
+                        title: new Text(asyncSnapshot.data[i]["title"],
+                          textDirection: TextDirection.rtl,),
+                        trailing: new CircleAvatar(
+                          backgroundImage: new NetworkImage(
+                              asyncSnapshot.data[i]["urlToImage"]),
+                        ),
+                        onTap: () {
+                          Navigator.push(context,
+                              new MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                  new DescriptionPage(asyncSnapshot.data[i])));
+                        },
+                      );
+                    });
+              }
+                },
+        ),
+
+        ),
+      appBar: new AppBar(title: new Text(appBarTitle)),
       drawer: new Drawer(
         child: ListView(
           children: <Widget>[
@@ -165,13 +139,27 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             ),
             new ListTile(
+              title: new Text('Egypt News'),
+              onTap: () {
+                Navigator.pop(context);
+                url = 'https://newsapi.org/v2/top-headlines?country=ar&apiKey=597ac250047845ad8e1fa41510638df2';
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (BuildContext context) => new MyApp())
+                );
+              },
+            ),
+            new ListTile(
               title: new Text('Saudi Arabia news'),
               onTap: () {
-                Navigator.of(context).pop();
+                Navigator.pop(context);
+                //appBarTitle = 'Saudi Arabia news';
+                url = "https://newsapi.org/v2/top-headlines?country=ar&apiKey=597ac250047845ad8e1fa41510638df2";
                 Navigator.push(
                   context,
                   new MaterialPageRoute(
-                      builder: (BuildContext context) => new ArticlesPage())
+                      builder: (BuildContext context) => new MyApp())
                 );
               },
             ),
@@ -179,11 +167,12 @@ class _MyHomePageState extends State<MyHomePage> {
               title: new Text('Argaam News'),
               onTap: () {
                 Navigator.pop(context);
-                //Navigator.of(context).pop();
+                //appBarTitle='Argaam News';
+                url = 'https://newsapi.org/v2/top-headlines?country=ar&apiKey=597ac250047845ad8e1fa41510638df2';
                 Navigator.push(
                     context,
                     new MaterialPageRoute(
-                        builder: (BuildContext context) => new ArgaamPage())
+                        builder: (BuildContext context) => new MyApp())
                 );
               },
             ),
@@ -208,5 +197,33 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+
+  void showAd () {
+
+    InterstitialAd myInterstitial = InterstitialAd(
+      // Replace the testAdUnitId with an ad unit id from the AdMob dash.
+      // https://developers.google.com/admob/android/test-ads
+      // https://developers.google.com/admob/ios/test-ads
+      adUnitId: adUnitId,
+      targetingInfo: targetingInfo,
+      listener: (MobileAdEvent event) {
+        print("InterstitialAd event is $event");
+      },
+    );
+    myInterstitial
+      ..load()
+      ..show(
+        anchorType: AnchorType.bottom,
+        anchorOffset: 0.0,
+      );
+
+    myInterstitial.dispose();
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
   }
 }
